6c378afceb2f6e97464f9b252d361d7039bd0221
Kanak Yadav <yadavkanak109@gmail.com>
2026-08-12 23:02:19 +0530

    refactor: enhance Intro component with improved text and styling

--- components/Intro/Intro.jsx
+++ components/Intro/Intro.jsx
@@ -4,16 +4,17 @@ const Intro = () => {
   return (
     <div className='w-full flex flex-col'>
      <div className='mb-6'  >
-      <h1>Kanak Yadav</h1>
+      <h1 className='font-bold'>Kanak Yadav</h1>
       <p>Full-stack Developer</p>
      </div>
 
-     <div className='flex flex-col gap-10'>
-       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, corrupti!
+     <div className='flex flex-col gap-5'>
+       <p>I’m a Full-Stack Developer focused on building clean, scalable, and user-friendly applications.
        </p>
-       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem reiciendis aperiam labore odit quasi soluta eligendi explicabo, nesciunt laborum ullam.</p>
-       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur repellendus excepturi aspernatur repellat et rem.</p>
-       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex nesciunt labore reprehenderit magni repudiandae nemo doloribus eaque molestiae rem, repellat, exercitationem dolorum esse corporis nostrum.</p>
+       <p>I’ve worked as a Backend Development Intern at <span className='font-bold'>
+Graphura India Private Limited</span>, building APIs and server-side applications.</p>
+       <p>I work primarily with <span className='font-bold'>Next.js</span> , <span className='font-bold'>React</span> , <span className='font-bold'>Node.js</span> , <span className='font-bold'>Express</span> , <span className='font-bold'>TypeScript</span> , <span className='font-bold'>MongoDB</span> , and REST APIs.</p>
+       <p>I enjoy building real-world products, backend systems, and AI-powered applications.</p>
      </div>
    
      
